59411ebc771dbc817140a05f4069550a84b2c424
Venkatesh Nyamagoudar <vnyamagoudar@gmail.com>
2023-08-01 15:22:18 -0400

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Venkatesh Nyamagoudar on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
